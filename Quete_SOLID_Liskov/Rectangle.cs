aae4197896b6addf5b323828582b4edf63166214
Sebastien David <sebastien.david08@gmail.com>
2021-12-06 10:34:23 +0100

    Ajoutez des fichiers projet.

---
+++ Quete_SOLID_Liskov/Rectangle.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quete_SOLID_Liskov
+{
+    public class Rectangle: Polygon
+    {
+        private double width;
+        private double height;
+
+        public void SetWidth(double width)
+        {
+            this.width = width;
+        }
+
+        public void SetHeight(double height)
+        {
+            this.height = height;
+        }
+
+        public double GetWidth()
+        {
+            return width;
+        }
+
+        public double GetHeight()
+        {
+            return height;
+        }
+
+        public override double Area()
+        {
+            return height * width;
+        }
+    }
+}
---
+++ Quete_SOLID_Liskov/Square.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quete_SOLID_Liskov
+{
+   public class Square: Polygon
+    {
+        private double side;
+        public void SetSide(double side)
+        {
+            this.side = side;
+        }
+
+        public double GetSide(double side)
+        {
+            return side;
+        }
+
+        public override double Area()
+        {
+            return side * side;
+        }
+    }
+}
